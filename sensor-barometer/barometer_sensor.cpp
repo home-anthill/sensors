701f7725431f06f1045c8469ee799c7c76767652
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2023-12-04 15:59:04 +0100

    feat: replace deprecated library for pressure sensor with the new one 'XENSIV Digital Pressure Sensor' and update libs and CI
    Signed-off-by: Stefano Cappa <stefano.cappa.ks89@gmail.com>

--- sensor-barometer/barometer_sensor.cpp
+++ sensor-barometer/barometer_sensor.cpp
@@ -4,28 +4,26 @@
 #include <Wire.h>
 
 // include specific libraries:
-// `DPS310-Pressure-Sensor` by Infineon (latest version on `dps310 branch` or
-// commit hash `ed02f803fc780cbcab54ed8b35dd3d718f2ebbda` from `https://github.com/Infineon/DPS310-Pressure-Sensor`).
-// You cannot use the one published on ArduinoIDE Library Manager, because it's outdated or not available.
-#include <Dps310.h>
+// `XENSIV Digital Pressure Sensor` by Infineon Technologies
+#include <Dps3xx.h>
 
 // Configure I2C GPIOs
 #define I2C_SDA 39
 #define I2C_SCL 40
 // global variables
-Dps310 Dps310PressureSensor = Dps310();
+Dps3xx Dps3xxPressureSensor = Dps3xx();
 
 void barometer_init_sensor() {
   Serial.println("barometer_init_sensor - called");
   Wire.setPins(I2C_SDA, I2C_SCL);
-  Dps310PressureSensor.begin(Wire);
+  Dps3xxPressureSensor.begin(Wire);
   Serial.println("barometer_init_sensor - sensor initialized successfully!");
 }
 
 float barometer_get_temperature() {
   Serial.println("barometer_get_temperature - called");
   float temperature;
-  if (Dps310PressureSensor.measureTempOnce(temperature) == 0) {
+  if (Dps3xxPressureSensor.measureTempOnce(temperature) == 0) {
     Serial.printf("barometer_get_temperature - temperature [°C]: %.2f\n", temperature);
     return temperature;
   }
@@ -35,7 +33,7 @@ float barometer_get_temperature() {
 float barometer_get_airpressure() {
   Serial.println("barometer_get_airpressure - called");
   float pressure;
-  if (Dps310PressureSensor.measurePressureOnce(pressure) == 0) {
+  if (Dps3xxPressureSensor.measurePressureOnce(pressure) == 0) {
     pressure = pressure / 1000;
     Serial.printf("barometer_get_airpressure - pressure [hPAa]: %.2f\n", pressure);
     return pressure;
